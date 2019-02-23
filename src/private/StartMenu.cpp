f8e9f8fcf96b6ac59119818aa4d087f23c9a997f
danicajiao <cajiao.dani@gmail.com>
2019-02-23 00:27:17 -0500

    implemented menu logic

--- src/private/StartMenu.cpp
+++ src/private/StartMenu.cpp
@@ -9,27 +9,12 @@ void initStartMenu()
 	printLogo();
 	printTitle();
 	this_thread::sleep_for(chrono::seconds(4));
-	cls(hStdout);	// clears console screen
-	
-	// TODO: create menu logic
-	runMainMenu();
-	int choice = getChoice();
 
-	if (choice == 1)
+	bool startFlag = 0;
+
+	while (startFlag == false)
 	{
-		runNewMenu(hStdout);
-	}
-	else if (choice == 2)
-	{
-		runLoadMenu(hStdout);
-	}
-	else if (choice == 3)
-	{
-		runCredits(hStdout);
-	}
-	else if (choice == 4)
-	{
-		exit(0);
+		runMainMenu(hStdout, startFlag);
 	}
 }
 
@@ -66,49 +51,206 @@ void printTitle()
 		 << endl;
 }
 
-void runMainMenu()
+void runMainMenu(HANDLE hStdout, bool &startFlag)
 {
+	cls(hStdout);	// clears console screen
 	printTitle();
 	cout << "[1] New\n"
 		 << "[2] Load\n"
 		 << "[3] Credits\n"
 		 << "[4] Quit\n"
 		 << endl;
+
+	int choice = getChoice(MenuType::Main);
+
+	switch (choice)
+	{
+	case 1:
+		runNewMenu(hStdout, startFlag);
+		break;
+	case 2:
+		runLoadMenu(hStdout, startFlag);
+		break;
+	case 3:
+		runCredits(hStdout);
+		break;
+	case 4:
+		runQuitMenu(hStdout, startFlag);
+		break;
+	default:
+		cout << "Error running menu" << endl;
+		break;
+	}
 }
 
-void runNewMenu(HANDLE hStdout)
+void runNewMenu(HANDLE hStdout, bool &startFlag)
 {
 	cls(hStdout);
-	cout << "\t\t\t\t                   - Start New Game -                 \n\n"
+	cout << "\t\t\t\t          ---------- Start New Game ----------       \n\n"
+		 << "[1] Yes\n"
+		 << "[2] No\n"
 		 << endl;
+
+	int choice = getChoice(MenuType::New);
+
+	if (choice == 1)
+	{
+		startFlag = true;
+	}
+	else
+	{
+		runMainMenu(hStdout, startFlag);
+	}
 }
 
-void runLoadMenu(HANDLE hStdout)
+void runLoadMenu(HANDLE hStdout, bool &startFlag)
 {
 	cls(hStdout);
-	cout << "\t\t\t\t                     - Load Game -                   \n\n"
-		<< endl;
+	cout << "\t\t\t\t             ---------- Load Game ----------         \n" << endl;
+
+	ifstream savefile;
+	savefile.open("saves.txt");
+
+	int count = 0;
+	string playername;
+	string date;
+	string time;
+	string meridiem;
+
+	cout << "[0] Cancel" << endl;
+
+	while (!savefile.eof())
+	{
+		savefile >> playername >> date >> time >> meridiem;
+		count++;
+		cout << "[" << count << "] " << playername << " " << date << " " << time << " " << meridiem << endl;
+	}
+	cout << endl;
+
+	savefile.close();
+
+	int choice = getLoadChoice(count);
+
+	if (choice == 0)
+	{
+		runMainMenu(hStdout, startFlag);
+	}
+	else
+	{
+		// TODO: write game start logic
+		startFlag = true;
+	}
 }
 
 void runCredits(HANDLE hStdout)
 {
 	cls(hStdout);
-	cout << "\t\t\t\t                      - Credits -                    \n\n"
-		<< endl;
+	cout << "\t\t\t\t              ---------- Credits ----------          \n\n"
+		 << "Designed and programmed by Daniel Cajiao\n"
+		 << "Inspired by Bethesda Game Studio's The Elder Scrolls V: Skyrim\n\n"
+		 << "[0] Back\n"
+		 << endl;
+
+	int choice = getChoice(MenuType::Credits);
+
+	if (true)
+	{
+
+	}
 }
 
-int getChoice()
+void runQuitMenu(HANDLE hStdout, bool &startFlag)
+{
+	cls(hStdout);
+	cout << "\t\t\t\t               ---------- Quit ----------            \n\n"
+		 << "[1] Yes\n"
+		 << "[2] No\n"
+		 << endl;
+
+	int choice = getChoice(MenuType::Quit);
+
+	if (choice == 1)
+	{
+		exit(0);
+	}
+	else
+	{
+		runMainMenu(hStdout, startFlag);
+	}
+}
+
+int getChoice(MenuType menu)
 {
 	int choice;
-	cout << "Select an option: ";
+	
+	if (menu == MenuType::Main)
+	{
+		cout << "Select a choice: ";
+		cin >> choice;
+		cout << endl;
+		while (choice < 1 || choice > 4)
+		{
+			cin.clear();	// clear bad input flag
+			cin.ignore(256, '\n');	// clear (up to 256) characters out of the buffer until a '\n' character is removed
+			cout << "Invalid option. Please enter a number from '1' to '4': ";
+			cin >> choice;
+		}
+	}
+	else if (menu == MenuType::New)
+	{
+		cout << "Start a new game?: ";
+		cin >> choice;
+		cout << endl;
+		while (choice < 1 || choice > 2)
+		{
+			cin.clear();	// clear bad input flag
+			cin.ignore(256, '\n');	// clear (up to 256) characters out of the buffer until a '\n' character is removed
+			cout << "Invalid option. Please enter either '1' or '2': ";
+			cin >> choice;
+		}
+	}
+	else if (menu == MenuType::Credits)
+	{
+		cout << "Enter '0' to return: ";
+		cin >> choice;
+		cout << endl;
+		while (choice != 0)
+		{
+			cin.clear();	// clear bad input flag
+			cin.ignore(256, '\n');	// clear (up to 256) characters out of the buffer until a '\n' character is removed
+			cout << "Invalid option. Please enter the number '0' to go back: ";
+			cin >> choice;
+		}
+	}
+	else if (menu == MenuType::Quit)
+	{
+		cout << "Are you should you want to quit?: ";
+		cin >> choice;
+		cout << endl;
+		while (choice < 1 || choice > 2)
+		{
+			cin.clear();	// clear bad input flag
+			cin.ignore(256, '\n');	// clear (up to 256) characters out of the buffer until a '\n' character is removed
+			cout << "Invalid option. Please enter either 1 or 2: ";
+			cin >> choice;
+		}
+	}
+	return choice;
+}
+
+int getLoadChoice(int count)
+{
+	cout << "Select a save to load: ";
+	int choice;
 	cin >> choice;
-	while (choice < 1 || choice > 4)
+	cout << endl;
+
+	while (choice < 0 || choice > count)
 	{
 		cin.clear();	// clear bad input flag
 		cin.ignore(256, '\n');	// clear (up to 256) characters out of the buffer until a '\n' character is removed
-		cout << "Invalid option. Please enter a number from 1 to 4: ";
+		cout << "Invalid option. Please enter a number from '0' to '" << count << "': ";
 		cin >> choice;
-		cout << endl;
 	}
 	return choice;
 }
--- src/public/StartMenu.h
+++ src/public/StartMenu.h
@@ -1,15 +1,28 @@
 #pragma once
 #include <stdlib.h>
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <thread>         // std::this_thread::sleep_for
 #include <chrono>         // std::chrono::seconds
 #include "ConsoleManagement.h"
 
+enum class MenuType
+{
+	Main,
+	New,
+	Credits,
+	Quit
+};
+
 void initStartMenu();
 void printLogo();
 void printTitle();
-void runMainMenu();
-void runNewMenu(HANDLE hStdout);
-void runLoadMenu(HANDLE hStdout);
+void runMainMenu(HANDLE hStdout, bool &startFlag);
+void runNewMenu(HANDLE hStdout, bool &startFlag);
+void runLoadMenu(HANDLE hStdout, bool &startFlag);
 void runCredits(HANDLE hStdout);
-int getChoice();
+void runQuitMenu(HANDLE hStdout, bool &startFlag);
+int getChoice(MenuType menu);
+int getLoadChoice(int count);
+
